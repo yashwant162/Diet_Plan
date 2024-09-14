3ab9729ebfd846f858142b5688ef639d0d9fe26d
Yashwant Singh <yashwantsingh162@gmail.com>
2024-09-14 17:24:39 +0530

    add login page design

--- diet_frontend/src/pages/IndexPage.jsx
+++ diet_frontend/src/pages/IndexPage.jsx
@@ -1,8 +1,10 @@
+import DietForm from "../components/DietForm";
+
 export default function IndexPage() {
   return (
     <>
-      <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-        <h1>Index Page</h1>
+      <div id="diet-analysis-form" className="mt-8">
+        <DietForm />
       </div>
     </>
   );
--- diet_frontend/src/pages/LoginPage.jsx
+++ diet_frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,102 @@
+import { useForm } from "react-hook-form";
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-        <h1>Login Page</h1>
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="./homepage_logo"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{" "}
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              start your 14-day free trial
+            </a>
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Name:
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="name"
+                {...register("name", { required: "Name is required" })}
+                className="appearance-none rounded-none relative block
+                w-full px-3 py-2 border border-gray-300
+                placeholder-gray-500 text-gray-900 rounded-t-md
+                focus:outline-none focus:ring-indigo-500
+                focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+              {errors.name && (
+                <p className="</Link>font">{errors.name.message}</p>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                    message: "Invalid email address",
+                  },
+                })}
+                className="appearance-none rounded-none relative block
+                w-full px-3 py-2 border border-gray-300
+                placeholder-gray-500 text-gray-900 rounded-b-md
+                focus:outline-none focus:ring-indigo-500
+                focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="email"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center
+              py-2 px-4 border border-transparent text-sm font-medium
+              rounded-md text-white bg-indigo-600 hover:bg-indigo-700
+              focus:outline-none focus:ring-2 focus:ring-offset-2
+              focus:ring-indigo-500"
+            >
+              Submit Details
+            </button>
+          </div>
+        </form>
       </div>
+    </div>
     </>
   );
 }
